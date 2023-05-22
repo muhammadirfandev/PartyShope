9811c24fce77e3d29341c6d073b413a0e83691d1
Irfan <mirzarfan17@gmail.com>
2023-05-21 22:50:30 -0700

    create PrivacyPolicy Page

--- src/Pages/TermsandConditions.jsx
+++ src/Pages/TermsandConditions.jsx
@@ -3,7 +3,7 @@ import { Navbar } from '../Components/Navbar/Navbar';
 import { TopBar } from '../Components/TopBar/TopBar';
 import { Footer } from '../Components/Footer/Footer';
 
-export const Termsandconditions=()=>{
+export const TermsandConditions=()=>{
 
 
     return(<>
