31a82c63906f31d19b2db94b7b285ee1a51f6c86
Irfan <mirzarfan17@gmail.com>
2023-05-22 22:41:24 -0700

    create contact us

---
+++ src/Pages/ContactUs.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const ContactUs = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
+
+  return (
+    <div className="contact-us">
+      <h2>Contact Us</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" required></textarea>
+        </div>
+        <button type="submit">Send Message</button>
+      </form>
+      <div className="contact-info">
+        <h3>Contact Information</h3>
+        <p>Phone: 1-800-123-4567</p>
+        <p>Email: info@partyshope.com</p>
+        <p>Address: 123 Party Street, City, Country</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
